bded40d47dd7d1fd3a7198cb16956754c17888fa
David Melendez <145343465+dmelnez@users.noreply.github.com>
2023-11-29 10:52:38 +0100

    Add files via upload

---
+++ Banco/Servicios/MenuImplementacion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+
+        /// <summary>
+        /// Metodo el cual Muestra al usuario un menu de seleccion
+        /// DMN - 21/11/2023
+        /// </summary>
+        /// <param name="args"></param>
+
+        public int menu()
+        {
+
+            Console.WriteLine("########## Seleccione una Opcion ##########");
+            Console.WriteLine("0 -> Finalizar la Aplicacion");
+            Console.WriteLine("1 -> Añadir un Nuevo usuario");
+            Console.WriteLine("2 -> Crear cuenta Bancaria");
+            Console.WriteLine("3 -> Modificar Datos de un Cliente");
+            Console.WriteLine("4 -> Eliminar Cuenta Bancaria");             
+            Console.WriteLine("###########################################");
+            int seleccionMenu = Convert.ToInt32(Console.ReadLine());
+            return seleccionMenu;
+
+        }
+
+
+
+        public string pedirDNI() {
+
+
+
+            Console.WriteLine("Indica el DNI del Cliente");
+            string dniCliente = Console.ReadLine();
+            return dniCliente;
+        }
+
+
+        public int modificarCliente()
+        {
+
+            Console.WriteLine("Introduzca el Campo que desea Modificar");
+            Console.WriteLine("1 -> ID");
+            Console.WriteLine("2 -> Nombre");
+            Console.WriteLine("3 -> Apellido");
+            Console.WriteLine("4 -> DNI");
+            Console.WriteLine("5 -> Fecha de Nacimiento");
+            Console.WriteLine("6 -> Email");
+            Console.WriteLine("7 -> Telefono");
+            Console.WriteLine("8 -> Fecha de Alta");
+            int modUsu = Convert.ToInt32(Console.ReadLine());
+            return modUsu;
+        }
+    }
+}
